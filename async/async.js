37810370bedc583c6abc5674f4ca4c6d2fbee752
Fatin Tawsif Hoque <fatintawsifhoque.fatin@gmail.com>
2025-11-12 22:28:57 +0600

    99

--- async/async.js
+++ async/async.js
@@ -8,5 +8,21 @@ async function fe() {
         console.log("error");
     }
 }
-
+ 
 fe();
+
+async function fe1() {
+    try {
+        let responsePost = await fetch("https://jsonplaceholder.typicode.com/posts/1");
+        let responseUser = await fetch("https://jsonplaceholder.typicode.com/users/1");
+        let dataPost = await responsePost.json();
+        let dataUser = await responseUser.json();
+        console.log(dataPost.title); 
+        console.log(dataUser.name); 
+    }
+    catch(err) {
+        console.log("error");
+    }
+}
+
+fe1();
