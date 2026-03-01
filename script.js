9c4aa41a6d04cf21b17a3925970580277d18aca6
Fatin Tawsif Hoque <168218628+fatintawsifhoque@users.noreply.github.com>
2026-03-01 19:07:31 +0600

    185

--- script.js
+++ script.js
@@ -1922,3 +1922,8 @@ function sumArray(arr) {
     return sum
 }
 console.log(sumArray([1, 2, 3, 4, 5]))
+
+function filterEven(arr) {
+    return arr.filter(el => el % 2 == 0)
+}
+console.log(filterEven([1, 2, 3, 4, 5, 6]))
