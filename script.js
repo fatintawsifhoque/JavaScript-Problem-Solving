45b810983d3d382aa078fe7e233b600eafd34c16
Fatin Tawsif Hoque <168218628+fatintawsifhoque@users.noreply.github.com>
2026-03-28 22:48:53 +0600

    199 - Add isAnagram function to check for anagrams

--- script.js
+++ script.js
@@ -2103,3 +2103,17 @@ function firstNonRepeatingChar(str) {
 }
 
 console.log(firstNonRepeatingChar("leetcode"));        
+
+function isAnagram(str, str2) {
+    if (str.length !== str2.length) {
+        return false;
+    }
+    
+    const sorted1 = str.split('').sort().join('');
+    const sorted2 = str2.split('').sort().join('');
+    
+    return sorted1 === sorted2;
+}
+
+console.log(isAnagram("listen", "silent")); 
+console.log(isAnagram("apple", "pale"));    
