553a98f67b5d6a5b2f2cc6354f6d474456d418df
Fatin Tawsif Hoque <168218628+fatintawsifhoque@users.noreply.github.com>
2026-03-30 19:41:26 +0600

    201 - Add function to count vowels in a string

--- script.js
+++ script.js
@@ -2127,3 +2127,15 @@ function isValidPalindrome(str) {
 console.log(isValidPalindrome("A man, a plan, a canal: Panama")); 
 console.log(isValidPalindrome("race a car"));
 console.log(isValidPalindrome("Madam")); 
+
+function countVowels(str) {
+     str = str.toLowerCase()
+    let count =0
+    for (let i = 0; i < str.length; i++){
+        if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u') {
+            count++
+        }
+    }
+    return count
+}
+console.log(countVowels("Hello"))
